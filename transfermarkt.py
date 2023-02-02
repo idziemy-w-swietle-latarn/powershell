f001292664e3692a40d2d815ddf93a81c6b0d43d
idziemy_w_swietle_latarn <idziemy_w_swietle_latarn@protonmail.com>
2023-02-02 17:29:24 +0100

    first commit

---
+++ transfermarkt.py
@@ -0,0 +1,38 @@
+main_leagues = {'La liga': 'https://www.transfermarkt.pl/laliga/gesamtspielplan/wettbewerb/ES1/saison_id/2022',
+                'Ekstraklasa': 'https://www.transfermarkt.pl/pko-ekstraklasa/gesamtspielplan/wettbewerb/PL1/saison_id/2022',
+                'Serie A': 'https://www.transfermarkt.pl/serie-a/gesamtspielplan/wettbewerb/IT1/saison_id/2022',
+                'Premier League': 'https://www.transfermarkt.pl/premier-league/gesamtspielplan/wettbewerb/GB1/saison_id/2022',
+}
+
+second_leagues = {'Fortuna 1 Liga':'https://www.transfermarkt.pl/fortuna-1-liga/gesamtspielplan/wettbewerb/PL2/saison_id/2022',
+                  'eWinner 2 Liga':'https://www.transfermarkt.pl/ewinner-2-liga/gesamtspielplan/wettbewerb/PL2L/saison_id/2022',
+                  'Bundesliga':'https://www.transfermarkt.pl/bundesliga/gesamtspielplan/wettbewerb/L1/saison_id/2022',
+                  'Ligue 1':'https://www.transfermarkt.pl/ligue-1/gesamtspielplan/wettbewerb/FR1/saison_id/2022',
+                  'Liga Portugal':'https://www.transfermarkt.pl/liga-portugal/gesamtspielplan/wettbewerb/PO1/saison_id/2022',
+                  'Eredivisie':'https://www.transfermarkt.pl/eredivisie/gesamtspielplan/wettbewerb/NL1/saison_id/2022',
+                  'Czeska Fortuna Liga': 'https://www.transfermarkt.pl/fortuna-liga/gesamtspielplan/wettbewerb/TS1/saison_id/2022',
+                  'Szkocka Premiership': 'https://www.transfermarkt.pl/scottish-premiership/gesamtspielplan/wettbewerb/SC1/saison_id/2022',
+                  'Süper Lig':'https://www.transfermarkt.pl/super-lig/gesamtspielplan/wettbewerb/TR1/saison_id/2022',
+                  'Jupiler Pro League':'https://www.transfermarkt.pl/jupiler-pro-league/gesamtspielplan/wettbewerb/BE1/saison_id/2022',
+                  'Grecka Super League 1':'https://www.transfermarkt.pl/super-league-1/gesamtspielplan/wettbewerb/GR1/saison_id/2022'}
+
+main_cups = {'Fortuna Puchar Polski':'https://www.transfermarkt.pl/fortuna-polnischer-pokal/gesamtspielplan/pokalwettbewerb/POPU/saison_id/2022',
+              'Puchar Włoch':'https://www.transfermarkt.pl/coppa-italia/gesamtspielplan/pokalwettbewerb/CIT/saison_id/2022',
+              'EFL Cup':'https://www.transfermarkt.pl/efl-cup/gesamtspielplan/pokalwettbewerb/CGB/saison_id/2022',
+              'FA Cup':'https://www.transfermarkt.pl/fa-cup/gesamtspielplan/pokalwettbewerb/FAC/saison_id/2022',
+              'Puchar Króla': 'https://www.transfermarkt.pl/copa-del-rey/gesamtspielplan/pokalwettbewerb/CDR/saison_id/2022'
+              }
+
+second_ccups = {'DFB Pokal':'https://www.transfermarkt.pl/dfb-pokal/gesamtspielplan/pokalwettbewerb/DFB/saison_id/2022',
+                'Coupe de France': 'https://www.transfermarkt.pl/coupe-de-france/gesamtspielplan/pokalwettbewerb/FRC/saison_id/2022',
+                'Puchar Portugalii': 'https://www.transfermarkt.pl/taca-de-portugal-placard/gesamtspielplan/pokalwettbewerb/POPO/saison_id/2022',
+                'Puchar Holandii': 'https://www.transfermarkt.pl/toto-knvb-beker/gesamtspielplan/pokalwettbewerb/NLP/saison_id/2022',
+                'Czeski MOL Cup': 'https://www.transfermarkt.pl/mol-cup/gesamtspielplan/pokalwettbewerb/TSP/saison_id/2022',
+                'Puchar Szkocji':'https://www.transfermarkt.pl/sfa-cup/gesamtspielplan/pokalwettbewerb/SFA/saison_id/2022',
+                'Kypello Elladas': 'https://www.transfermarkt.pl/kypello-elladas/gesamtspielplan/pokalwettbewerb/GRP/saison_id/2022',
+                
+                'Puchar Belgii':'https://www.transfermarkt.pl/beker-van-belgie/gesamtspielplan/pokalwettbewerb/CCB/saison_id/2022',}
+
+eurocups = {'Liga Konferencji Europy': 'https://www.transfermarkt.pl/uefa-europa-conference-league/gesamtspielplan/pokalwettbewerb/UCOL/saison_id/2022',
+            'Liga Europy': 'https://www.transfermarkt.pl/europa-league/gesamtspielplan/pokalwettbewerb/EL/saison_id/2022',
+            'Liga Mistrzów':'https://www.transfermarkt.pl/uefa-champions-league/gesamtspielplan/pokalwettbewerb/CL/saison_id/2022'}
---
+++ transfermarkt_parsing.py
@@ -0,0 +1,137 @@
+import requests
+from bs4 import BeautifulSoup
+from transfermarkt import main_leagues, second_leagues
+from datetime import date, datetime
+from meczbot import subreddit
+import logging
+
+logging.basicConfig(filename='failed_extractions.log', encoding='utf-8', level=logging.WARN)
+
+class FutureGame(Exception):
+    "Raised when game will be played tomorrow or later"
+    pass
+
+class NoDate(Exception):
+    "Raised when game date was not in a.tag and date from previous game wasn't provided"
+    pass
+
+def title_text(competition_name):
+    return competition_name + ' ' + str(date.today())
+
+def body_text(games: list[dict]):
+    games = [str(match) for match in games]
+    return '\n\n'.join(games)
+    
+def is_theDay(checked_date_string:date, theDay:date):
+    return datetime.strptime(checked_date_string, '%d.%m.%Y').date() == theDay
+    
+def is_today(checked_date_string:date):
+    result = is_theDay(checked_date_string, date.today())
+    return result
+
+def is_past(date_string):
+    return datetime.strptime(date_string, '%d.%m.%Y').date() < date.today()
+    
+def is_future(date_string):
+    return datetime.strptime(date_string, '%d.%m.%Y').date() > date.today()
+
+class Mother():
+    pass
+
+class Domestic_league():
+
+    def __init__(self) -> None:
+        self.helper_date = None
+        self.helper_time = None
+
+    @staticmethod
+    def oneSeason_gamedays(soup):
+        gamedays = soup.body.div.main.find_all('div', {'class': 'content-box-headline'})
+        return [gameday.parent for gameday in gamedays]
+    
+    @staticmethod        
+    def oneDay_games(gameday):
+        gameday = gameday.table.tbody.find_all('tr')
+        return [game.find_all('td') for game in gameday if not game.has_attr('class')] #9 matches 
+    
+    def todays_game(self, game):
+        game_dict = {}
+        if game[0].a:
+            game_date = game[0].a.text
+            game_dict['date'] = game_date
+            self.helper_date = game_date
+        else:
+            if self.helper_date:
+                game_dict['date'] = self.helper_date
+            else:
+                logging.warning('Failed, no date available:{}'.format(str(game)))
+                return False
+        if not is_today(game_dict['date']):
+            return False
+            
+        game_time = game[1].text.strip()
+        if game_time:
+            game_dict['time'] = game_time
+            self.helper_time = game_time
+        else:
+            if self.helper_time:
+                game_dict['time'] = self.helper_time
+            else:
+                logging.warning('Failed, no time available:{}'.format(str(game)))
+                return False
+                
+        game_dict['host'] = game[2].a.text.strip()
+        game_dict['guest'] = game[6].a.text.strip()
+        
+        assert game_dict['date']
+        assert game_dict['guest']
+        assert game_dict['host']
+        assert game_dict['time']
+        
+        return game_dict
+
+# test_league = 'Fortuna Liga - Łączny terminarz Transfermarkt.html'
+# with open(test_league) as tp:
+#     soup = BeautifulSoup(tp, 'html.parser')
+#     a = Domestic_league()
+#     for gameday in a.oneSeason_gamedays(soup):
+#         for game in a.oneDay_games(gameday):
+#             today = a.todays_game(game)
+#             if today:
+#                 print(today)
+
+   
+headers = {
+'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36',
+}
+
+for name, link in main_leagues.items():
+    r = requests.get(link, headers=headers)
+    soup = BeautifulSoup(r.text, 'html.parser')    
+    a = Domestic_league()
+    matches = []
+    for gameday in a.oneSeason_gamedays(soup):
+        for game in a.oneDay_games(gameday):
+            today = a.todays_game(game)
+            if today:
+                matches.append(today)
+                
+    # if matches:
+    #     subreddit.submit(title_text(name), selftext = body_text(matches), discussion_type='CHAT')
+
+matches = []
+for name, link in second_leagues.items():
+    submatches = []
+    r = requests.get(link, headers=headers)
+    soup = BeautifulSoup(r.text, 'html.parser')    
+    a = Domestic_league()
+    for gameday in a.oneSeason_gamedays(soup):
+        for game in a.oneDay_games(gameday):
+            today = a.todays_game(game)
+            if today:
+                submatches.append(today)
+    if submatches:
+        matches.append(15*'*' + '   ' + name + '  ' + 15*'*')
+        matches.extend(submatches)
+
+subreddit.submit(title_text('Inne ligi'), selftext = body_text(matches), discussion_type='CHAT')
